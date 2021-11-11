85815257cd709dae0a2c096638566ebfab8218cc
Daniel <daniel@test.com>
2021-11-11 09:40:59 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import Map from "./components/Map";
+import Loader from "./components/Loader";
+import Header from "./components/Header";
+
 const App = () => {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 };
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import spinner from "../img/spinner.gif";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={spinner} alt="" />;<h1>Fetching Data</h1>
+    </div>
+  );
+};
+
+export default Loader;
